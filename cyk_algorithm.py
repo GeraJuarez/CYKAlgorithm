5e3443d3fd68a8f8d79a06f730adb8def3873f69
geraJuarez <geratrex781@gmail.com>
2017-09-29 21:29:52 -0500

    Testing funcio9nality

--- cyk_algorithm.py
+++ cyk_algorithm.py
@@ -1,23 +1,86 @@
 
-# Initialize data structures
+""" Initialize data structures """
 CFG = {}    # Context Free Grammar
 CYK = []    # CYK Matrix
 
+""" Auxiliar Methods """
 def print_matrix( cyk ):
+    """ Print current state of the Matrix """
     for row in cyk:
-        print " ".join( row )
+        print row
+    print "=" * 20
 
-def populate_matrix( cyk ):
-    word = raw_input("Insert string to verify whether it belongs to the CFG: ")
-    for i in range( len(word) ):
-        cyk.append( ["-"] * len(word) )
+def populate_matrix( cyk, w ):
+    """ Initialize Matrix """
+    for i in range( len(w) ):
+        cyk.append( ["0"] * len(w) )
 
 def print_grammar( cfg ):
+    """ Visualize the Grammar defined by the user """
     print "==========\n"
     for k in cfg.keys():
         print k + " -> " + "|".join (cfg[k] )
     print "\n=========="
 
+def in_grammar( letter ):
+    """ Check whether the letter can be obtained by a non terminal symbol """
+    temp = []
+    for k in CFG.keys():
+        if letter in CFG[k]:
+            temp.append(k)
+    return temp
+
+def cyk_algorithm_step1( word, cyk ):
+    """ Check each character of the string if it can be produced by the CFG """
+    i = j = 0
+    for c in word:
+        non_term_symbols = in_grammar(c)
+        if non_term_symbols:
+            cyk[i][j] = non_term_symbols
+        else:
+            cyk[i][j] = []
+        i, j = i + 1, j + 1
+
+def cyk_algorithm( word, cyk ):
+    cyk_algorithm_step1( word, cyk )
+    # Iterate over matrix
+    tempList = []
+    for k in range(1, len(word) ):
+        print_matrix( cyk )
+        i = j = 0
+        tempList = []
+        while j < len(word) - 1:
+            if j + 1 == len(word) - 1:
+                tempList = []
+            if j == k + i:
+                i += 1
+                j = i
+            for m in cyk[i][j]:
+                for n in cyk[j + 1][k + i]:
+                    print m + n
+                    posible_symbols = in_grammar( m + n )
+                    print posible_symbols
+                    tempList = list( set(tempList) | set(posible_symbols) )
+                    print tempList
+            if tempList:
+                cyk[i][k + i] = tempList
+            j += 1
+"""
+for k in range(1, 6):
+    i = 0
+    j = 0
+    print "============"
+    while j < 6 - 1:
+        if j == k + i:
+            i += 1
+            j = i
+        print str(i) + " : " + str(j) + "\t"
+        print str(j + 1) + " : " + str(k + i)
+        print "--------------"
+        j += 1
+"""
+
+""" Main """
 n = raw_input("Insert number of Non-terminal Symbols of the CFG: ")
 print
 for i in range ( int(n) ):
@@ -28,6 +91,9 @@ for i in range ( int(n) ):
     CFG[x] = y.split("|")
 
 print_grammar( CFG )
-populate_matrix( CYK )
+word = raw_input("Insert string to verify whether it belongs to the CFG: ")
+populate_matrix( CYK, word )
+cyk_algorithm( word, CYK )
 print_matrix( CYK )
+
 
